cb74b4efc4f443d66b5534f54cdb411fa911b220
RPMorrigan <morriganpriscilla@gmail.com>
2026-04-22 13:36:02 -0600

    getting ready for B&B

--- client/src/pages/AllCards.jsx
+++ client/src/pages/AllCards.jsx
@@ -1,5 +1,23 @@
 export default function AllCards() {
+
+const cardFiles = [
+  'O.jpg','I.jpg','II.jpg','III.jpg','IV.jpg','V.jpg','VI.jpg',
+  'VII.jpg','VIII.jpg','IX.jpg','X.jpg','XI.jpg','XII.jpg',
+  'XIII.jpg','XIV.jpg','XV.jpg','XVI.jpg','XVII.jpg','XVIII.jpg',
+  'XIX.jpg','XX.jpg','XXI.jpg'
+];
     return (
-        <></>
+        <main>
+            <h1>The Cards</h1>
+                <div className="cards-grid">
+                    {cardFiles.map((file) => (
+                    <img
+                    key={file}
+                    src={`/tarot_cards/${file}`}
+                    alt={file.replace('.jpg', '')}
+                    />
+                    ))}
+                </div>
+        </main>
     )
 }
